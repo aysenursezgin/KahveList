b3081425c6ffad9794235d31940dfaa2b829502e
aysenursezgin <115412288+aysenursezgin@users.noreply.github.com>
2024-10-02 09:27:59 +0300

    Proje dosyası ekle.

---
+++ KahveList/Program.cs
@@ -0,0 +1,23 @@
+﻿// Kahve isimlerini tutacak bir liste oluşturma
+
+        List<string> kahveListesi = new List<string>();
+
+// Kullanıcıdan 5 kahve ismi alma
+
+Console.WriteLine("Lütfen 5 kahve ismi girin:");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Kahve {i + 1}: ");
+    string kahve = Console.ReadLine();
+    kahveListesi.Add(kahve);      // Listeye kahve ismini ekle
+}
+
+
+// Kahve isimlerini yazdırma
+
+Console.WriteLine("\n Kahve Listesi:");
+foreach (var kahve in kahveListesi)
+{
+    Console.WriteLine(kahve);
+}
